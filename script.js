98b151aba009ece0da783e3d3db99c1d94cb4da9
vikrantsrgc <145971065+vikrantsrgc@users.noreply.github.com>
2025-04-08 11:21:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+let counter = 0 ;
+
+let displyCount = document.getElementById("counter");
+let incrementBtn = document.getElementById("incrementBtn")
+
+
+function UpdateDisplay() {
+	displyCount.textContent = counter 
+}
+
+incrementBtn.addEventListener("click",()=>{
+	counter++
+	UpdateDisplay()
+})
